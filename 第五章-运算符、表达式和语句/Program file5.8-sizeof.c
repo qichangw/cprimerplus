9f15752b033aa2151f02ff966fa3a6754b0262be
启昌 王 <1635298187@qq.com>
2022-10-01 11:00:24 +0800

    qichangw-commit-workto100

---
+++ 第五章-运算符、表达式和语句/Program file5.8-sizeof.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int n = 0;
+	size_t intsize;
+
+	intsize = sizeof(int);
+	printf("n=%d,n has %zd bytes;all ints have %zd bytes.\n",
+		n, sizeof n, intsize);
+	
+	getchar();
+	return 0;
+}
