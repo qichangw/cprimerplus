5672a99cd7ddca28433fe3255ad8c4bba4cc44b9
启昌 王 <1635298187@qq.com>
2022-10-29 20:53:57 +0800

    qichangw-commit-work291

---
+++ 第十一章：字符串和字符串函数/Program file11.13-nono.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+	char side_a[] = "Side A";
+	char dont[] = { 'W','O','W','!' };
+	char side_b[] = "Side B";
+
+	puts(dont);
+
+	return 0;
+}
---
+++ 第十一章：字符串和字符串函数/Program file11.14-put1.c
@@ -0,0 +1,8 @@
+//不添加\n的puts函数
+#include<stdio.h>
+void put1(const char* string) {
+	while (*string != '\0')
+		putchar(*string++);
+
+
+}
